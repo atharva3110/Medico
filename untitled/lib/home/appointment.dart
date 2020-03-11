b01f801e636b063139c7596cdf68572911dce966
atharva3110 <47656352+atharva3110@users.noreply.github.com>
2020-03-11 07:16:44 +0530

    my appointments screen

--- untitled/lib/home/appointment.dart
+++ untitled/lib/home/appointment.dart
@@ -17,6 +17,9 @@ class _appointmentState extends State<appointment> {
   @override
   Widget build(BuildContext context) {
     var divheight = MediaQuery.of(context).size.height;
+    final appointments=['March 2020','March 2020','March 2020','Feb 2020','May 2020'];
+    final appointment_dates=['16','17','22','31','22'];
+    final appointment_days=['Saturday','Monday','Friday','Wednesday','Thursday'];
     return Scaffold(
       key: _scaffoldKey,
       body: Column(
@@ -28,7 +31,7 @@ class _appointmentState extends State<appointment> {
                     image: new AssetImage('assets/login/header.jpg'),
                     fit: BoxFit.cover)
             ),
-            height: divheight/2*0.4,
+            height: divheight/2*0.35,
             child: new Stack(
                 children: <Widget>[
                   Column(
@@ -58,16 +61,115 @@ class _appointmentState extends State<appointment> {
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               new Container(
-                height: 30,
+                height: 40,
                 child: new Text("Michael Scott",
                   style: TextStyle(
                       fontSize: 25.0,fontWeight: FontWeight.bold,color: Colors.black,backgroundColor: Colors.white
                   ),),
               ),
             ],
+          ),
+          new Container(
+            height: 30,
+            child: new Text("My Appointments",
+              style: TextStyle(
+                  fontSize: 20.0,fontWeight: FontWeight.normal,color: Colors.black,backgroundColor: Colors.white
+              ),),
+          ),
+          Container(child: Divider(color: Color.fromRGBO(181, 166, 166, 1),),
+            padding:EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0) ,),
+          new Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.fromLTRB(20.0, 5.0, 5.0, 0.0),
+            height: 25,
+            child: new Text("Upcoming",textAlign: TextAlign.left,
+              style: TextStyle(
+                  fontSize: 15.0,fontWeight: FontWeight.normal,color: Colors.black,backgroundColor: Colors.white
+              ),),
+          ),
+          Container(
+            height: 180,
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: appointments.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(appointments[index],style: TextStyle(
+                      color: Color(0xFFB5A6A6),
+                    ),),
+                    subtitle: Text(appointment_days[index],style:TextStyle(
+                      color: Color(0xFFB5A6A6),
+                    ),),
+                    leading: Text(appointment_dates[index],
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.black,
+                      ),),
+                    trailing: RaisedButton(
+                      color: Color(0xFFC81010),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                      ),
+                      onPressed: (){},
+                      child: Text("More Details",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),),
+                    ),
+
+                  ),
+                );
+              },
+                ),
+          ),
+          SizedBox(height: 20.0,),
+          new Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.fromLTRB(20.0, 5.0, 5.0, 0.0),
+            height: 25,
+            child: new Text("Recent",textAlign: TextAlign.left,
+              style: TextStyle(
+                  fontSize: 15.0,fontWeight: FontWeight.normal,color: Colors.black,backgroundColor: Colors.white
+              ),),
+          ),
+          Container(
+            height: 90,
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: appointments.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(appointments[index],style: TextStyle(
+                      color: Color(0xFFB5A6A6),
+                    ),),
+                    subtitle: Text(appointment_days[index],style:TextStyle(
+                      color: Color(0xFFB5A6A6),
+                    ),),
+                    leading: Text(appointment_dates[index],
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.black,
+                      ),),
+                    trailing: RaisedButton(
+                      color: Color(0xFFC81010),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                      ),
+                      onPressed: (){},
+                      child: Text("More Details",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),),
+                    ),
+
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
